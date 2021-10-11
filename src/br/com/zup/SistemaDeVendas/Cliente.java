695845823365189fc04c7b96e03630d9efc4b5fe
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-11 12:34:07 -0300

    Arrumar metodo toString

--- src/br/com/zup/SistemaDeVendas/Cliente.java
+++ src/br/com/zup/SistemaDeVendas/Cliente.java
@@ -8,9 +8,9 @@ public class Cliente extends Pessoa {
     @Override
     public String toString() {
         StringBuilder retorno = new StringBuilder();
-        retorno.append("\nDigite o nome do cliente: " + getNome());
-        retorno.append("\nDigite o CPF do cliente: " + getCPF());
-        retorno.append("\nDigite o e-mail do cliente: " + getEmail());
+        retorno.append("\nNome do cliente: " + getNome());
+        retorno.append("\nCPF do cliente: " + getCPF());
+        retorno.append("\nE-mail do cliente: " + getEmail());
         return retorno.toString();
     }
 }
