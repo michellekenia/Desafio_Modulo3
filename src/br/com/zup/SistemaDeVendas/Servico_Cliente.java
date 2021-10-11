90380f235f5be77b98d08de87e9c14b791d00a38
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-11 16:42:38 -0300

    Criar metodo verificar @

--- src/br/com/zup/SistemaDeVendas/Servico_Cliente.java
+++ src/br/com/zup/SistemaDeVendas/Servico_Cliente.java
@@ -5,17 +5,25 @@ import java.util.List;
 
 public class Servico_Cliente {
 
-
     private static List<Cliente> listaDeClientes = new ArrayList<>();
+
+    public static List<Cliente> getListaDeClientes() {
+        return listaDeClientes;
+    }
+
+    public static void validarEmail(String email) throws Exception {
+        if (!email.contains("@")) {
+            throw new Exception("E-mail inválido.");
+        }
+
+    }
+
+
 
     public static Cliente cadastrarCliente(String nome, String CPF, String email) {
         Cliente cliente = new Cliente(nome, CPF, email);
         listaDeClientes.add(cliente);
         return cliente;
-    }
-
-    public static List<Cliente> getListaDeClientes() {
-        return listaDeClientes;
     }
 
     public static void exibirListaDeClientes() {
