59cb393d3481f55d33b6cc92aa8767fa7e7810ce
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-11 12:02:38 -0300

    Retirar lista de cliente duplicada

--- src/br/com/zup/SistemaDeVendas/Servico_Cliente.java
+++ src/br/com/zup/SistemaDeVendas/Servico_Cliente.java
@@ -5,14 +5,13 @@ import java.util.List;
 
 public class Servico_Cliente {
 
-    private static List<Cliente> cliente = new ArrayList<>();
 
-        private static List<Cliente> listaDeClientes = new ArrayList<>();
+    private static List<Cliente> listaDeClientes = new ArrayList<>();
 
-        public static Cliente cadastrarCliente(String nome, String CPF, String email) {
-            Cliente cliente = new Cliente(nome, CPF, email);
-            listaDeClientes.add(cliente);
-            return cliente;
-        }
-
+    public static Cliente cadastrarCliente(String nome, String CPF, String email) {
+        Cliente cliente = new Cliente(nome, CPF, email);
+        listaDeClientes.add(cliente);
+        return cliente;
     }
+
+}
