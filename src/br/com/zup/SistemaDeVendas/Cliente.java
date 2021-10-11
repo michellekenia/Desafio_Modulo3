e38cd5d324613b064c4b6cedac60e935b09d06cf
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-11 11:39:42 -0300

    Modelar Cliente

--- src/br/com/zup/SistemaDeVendas/Cliente.java
+++ src/br/com/zup/SistemaDeVendas/Cliente.java
@@ -1,4 +1,16 @@
 package br.com.zup.SistemaDeVendas;
 
-public class Cliente {
+public class Cliente extends Pessoa {
+    public Cliente(String nome, String CPF, String email) {
+        super(nome, CPF, email);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\nDigite o nome do cliente: " + getNome());
+        retorno.append("\nDigite o CPF do cliente: " + getCPF());
+        retorno.append("\nDigite o e-mail do cliente: " + getEmail());
+        return retorno.toString();
+    }
 }
