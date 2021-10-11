3c6cf1f14c4bfc7a6450a87d7fd2de87b27870db
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-11 11:38:04 -0300

    Modelar Vendedor

--- src/br/com/zup/SistemaDeVendas/Vendedor.java
+++ src/br/com/zup/SistemaDeVendas/Vendedor.java
@@ -1,4 +1,17 @@
 package br.com.zup.SistemaDeVendas;
 
-public class Vendedor {
+public class Vendedor extends Pessoa {
+
+    public Vendedor(String nome, String CPF, String email) {
+        super(nome, CPF, email);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\nDigite o nome do vendedor: " + getNome());
+        retorno.append("\nDigite o CPF do vendedor: " + getCPF());
+        retorno.append("\nDigite o e-mail do vendeor: " + getEmail());
+        return retorno.toString();
+    }
 }
