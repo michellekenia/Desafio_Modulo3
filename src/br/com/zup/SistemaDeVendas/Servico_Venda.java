0ebba509416a0bfaec3c4e0044ec3f78620a10a8
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-11 13:04:53 -0300

    Criar metodo cadastrar venda

--- src/br/com/zup/SistemaDeVendas/Servico_Venda.java
+++ src/br/com/zup/SistemaDeVendas/Servico_Venda.java
@@ -1,2 +1,17 @@
-package br.com.zup.SistemaDeVendas;public class Servico_Venda {
+package br.com.zup.SistemaDeVendas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Servico_Venda {
+
+    private static List<Venda> listaDeVendas = new ArrayList<>();
+
+    public static Venda cadastrarVenda(Cliente cliente,Vendedor vendedor, double valor, String data) {
+        Venda venda = new Venda(vendedor, cliente,valor,data);
+        listaDeVendas.add(venda);
+        return venda;
+    }
+
+
 }
