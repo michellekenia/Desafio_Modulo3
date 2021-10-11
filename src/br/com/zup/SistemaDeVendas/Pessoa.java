b33798652b59304d7b7835b8e16be2dd0d29e62e
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-11 11:18:26 -0300

    Modelar classe Pessoa

--- src/br/com/zup/SistemaDeVendas/Pessoa.java
+++ src/br/com/zup/SistemaDeVendas/Pessoa.java
@@ -1,4 +1,46 @@
 package br.com.zup.SistemaDeVendas;
 
-public class Pessoa {
+public class asbtract Pessoa {
+    String nome;
+    String CPF;
+    String email;
+
+    public asbtract(String nome, String CPF, String email) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\nDigite o nome: " + nome);
+        retorno.append("\nDigite o CPF: " + CPF);
+        retorno.append("\nDigite o e-mail: " + email);
+        return retorno.toString();
+    }
 }
