c195f48251c0f652646149a4715cc6662ab6dc7a
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-11 11:47:34 -0300

    Criar método cadastrar cliente

--- src/br/com/zup/SistemaDeVendas/Servico_Cliente.java
+++ src/br/com/zup/SistemaDeVendas/Servico_Cliente.java
@@ -1,4 +1,17 @@
 package br.com.zup.SistemaDeVendas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Servico_Cliente {
+
+    private List<Cliente> listaDeClientes = new ArrayList<>();
+
+    public void cadastrarCliente(String nome, String CPF, String email) {
+        Cliente cliente = new Cliente();
+        listaDeClientes.add(cliente);
+
+    }
+
+
 }
